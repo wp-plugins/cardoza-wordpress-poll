6444d4a33c671e2714d78e9d06057c39a7e8a347
vinoj.cardoza <vinoj.cardoza@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-26 12:52:06 +0000

    git-svn-id: https://plugins.svn.wordpress.org/cardoza-wordpress-poll/tags/31.05.2012@564009 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ app/views/CWPViewManagePolls.class.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * Filename : CWPViewManagePolls
+ * This is will create an interface to manage polls
+ */
+
+class CWPViewManagePolls{
+
+    function init() {?>
+
+        <div id="all-polls">
+            <div id="tab1" class="tab-content">
+                <div id="manage-polls">
+                <input id="refresh-list" class="inpt"
+                    onclick="javascript:refreshPollList()"
+                    onblur="javascript:setBorderDefault('refresh-list');" 
+                    onfocus="javascript:setBorder('refresh-list');" type="button" value="Refresh Poll List"/>
+                <input id="edit-poll" class="inpt"
+                    onblur="javascript:setBorderDefault('edit-poll');" 
+                    onfocus="javascript:setBorder('edit-poll');" type="button" value="Edit a Poll" onclick="javascript:editPoll()" />
+                <input id="delete-poll" class="inpt"
+                    onblur="javascript:setBorderDefault('delete-poll');" 
+                    onfocus="javascript:setBorder('delete-poll');" type="button" value="Delete a Poll" onclick="javascript:deletePoll()" />
+                <br />     
+                <div id="all-polls">
+                <table width="100%" style="background-color: #4A7194;color:#333;">
+                    <thead style="background-color: #4A7194;color:#FFF;height:30px;">
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Question</th>
+                        <th>Answer type</th>
+                        <th>Start date</th>
+                        <th>End date</th>
+                        <th>Status</th>
+                        <th>Total votes</th>
+                        <th>View Results</th>
+                    </thead>
+                    <form id="manage-poll">
+                        <?php
+                        $controller = new CWPController();
+                        $polls = $controller->getPollList();
+                        $total_votes = 0;
+                        foreach($polls as $poll){?>
+
+                            <tr style="background-color: #ECF1EF;height:40px;">
+                                <td align="center"><?php print $poll->id;?></td>
+                                <td style="padding-left:3px;"><?php print $poll->name;?></td>
+                                <td style="padding-left:3px;"><?php print $poll->question;?></td>
+                                <td style="padding-left:3px;"><?php print $poll->answer_type;?></td>
+                                <td align="center"><?php print $poll->start_date;?></td>
+                                <td align="center"><?php print $poll->end_date;?></td>
+                                <td align="center">
+                                <?php
+                                $timestamp = $controller->getStrToTime($poll->end_date);
+                                $current_time = time();
+                                if($current_time < $timestamp) echo "Open";
+                                else echo "Closed"
+                                ?>
+                                </td>
+                                <td align="center"><?php print $poll->total_votes;?></td>
+                                <td align="center"><input name="view_poll_results" type="button" onclick="javascript:viewPollResults(<?php print $poll->id;?>)" value="View Result"/></td>
+                            </tr>
+                        <?php
+                        $total_votes = $total_votes +  $poll->total_votes;
+                        }
+                        ?>
+                    </form>
+                </table>
+                    <br/>
+                <div id="box">
+                <div id="label" style="color:#000;">Total No of Voters :</div>&nbsp;&nbsp;&nbsp;<?php print $total_votes;?>
+                <div id="clear"></div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+<?php }
+}
+?>
---
+++ app/views/CWPViewWidget.class.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Filename : CWPViewWidget.class.php
+ * This will display the options of widget for poll
+ */
+class CWPViewWidget{
+    
+    function __construct() {
+        $controller = new CWPController();
+        $opts = $controller->cwpp_options();
+        
+        ?>
+        <div id="tab4" class="tab-content">
+            <h3>Widget options</h3>
+            <div id="box">
+                <form id="widget-options">
+                <div id="label">Title of the widget: </div>
+                <input id="widget-title" style="width: 300px;"
+                       onblur="javascript:setBorderDefault('widget-title');" 
+                       onfocus="javascript:setBorder('widget-title');"
+                       name="widget_title" type="text" value="<?php if(!empty($opts['title'])) print $opts['title'];?>" class="txt" />
+                <div id="clear"></div>
+                <div id="label">Display Older Polls Link: * </div>
+                <select class="slct" name="poll-archive" style="width:75px;">
+                    <option value="yes"
+                    <?php
+                        if(!empty($opts['archive']) && $opts['archive']=='yes') echo " selected";
+                    ?>
+                    >Yes</option>
+                    <option value="no"
+                    <?php
+                        if(!empty($opts['archive']) && $opts['archive']=='no') echo " selected";
+                    ?>
+                    >No</option>
+                </select>
+                <div id="clear"></div>
+                <div id="label">Select the latest number of polls to be displayed: * </div>
+                <select class="slct" name="no_of_polls" style="width:75px;">
+                    <?php
+                        for($i=1; $i<=10; $i++){
+                            echo "<option value=".$i;
+                            if(!empty($opts['no_of_polls']) && $opts['no_of_polls']==$i) echo " selected";
+                            echo ">".$i."</option>";
+                        }
+                    ?>
+                </select>
+                <div id="clear"></div>
+                <div id="label">Widget width: </div>
+                <input id="widget-width" style="width: 50px;"
+                       onblur="javascript:setBorderDefault('widget-width');" 
+                       onfocus="javascript:setBorder('widget-width');"
+                       name="widget_width" type="text" value="<?php if(!empty($opts['width'])) print $opts['width'];?>" class="txt" />px
+                <div id="clear"></div>
+                
+                <div id="label">Widget height: </div>
+                <input id="widget-height" style="width: 50px;"
+                       onblur="javascript:setBorderDefault('widget-height');" 
+                       onfocus="javascript:setBorder('widget-height');"
+                       name="widget_height" type="text" value="<?php if(!empty($opts['height'])) print $opts['height'];?>" class="txt" />px
+                <div id="clear"></div>
+                <center>
+                <input type="hidden" name="action" value="save_widget" />
+                <input id="widget-opt" 
+                       onclick="javascript:saveWidgetOptions()"
+                       onblur="javascript:setBorderDefault('widget-opt');" 
+                       onfocus="javascript:setBorder('widget-opt');"
+                       type="button" value="Save Widget Options"/></center>
+                </form>
+            </div>
+        </div>
+<?php
+        
+    }
+
+}
+?>
