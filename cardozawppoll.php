0dab705bc736b028d39d4a92da27f6346255536c
vinoj.cardoza <vinoj.cardoza@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-26 13:32:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/cardoza-wordpress-poll/trunk@564033 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cardozawppoll.php
+++ cardozawppoll.php
@@ -3,7 +3,7 @@
 Plugin Name: Cardoza Wordpress Poll
 Plugin URI: http://fingerfish.com/cardoza-wordpress-poll
 Description: Cardoza Wordpress Poll is completely ajax powered polling system. This poll plugin supports both single and multiple selection of answers.
-Version: 26.06.2012
+Version: 2.0
 Author: Vinoj Cardoza
 Author URI: http://fingerfish.com/about-me/
 License: GPL2
